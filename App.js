910805aa5e9fb42ae806b45cf1d4575b3532ce83
Tony-Starkus <thalisson.bandeira@hotmail.com>
2022-03-12 11:15:33 -0500

    refactor :recycle: configured tab bar and header styles

--- App.js
+++ App.js
@@ -12,11 +12,15 @@ import {
   StatusBar,
   StyleSheet,
   useColorScheme,
+  View,
 } from 'react-native';
 
 // React navigation
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// Icons
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -41,7 +45,52 @@ const App = () => {
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              // Set icons for each screen
+              let iconName;
+              let colorIcon = focused ? '#FFF' : '#8F8F8F';
+              if (route.name === 'Carteiras') {
+                iconName = 'wallet-outline';
+              } else if (route.name === 'Portfolio') {
+                iconName = 'bar-chart-outline';
+              } else if (route.name === 'Investir') {
+                iconName = 'logo-usd';
+              } else if (route.name === 'Conteúdos') {
+                iconName = 'document-text-outline';
+              } else if (route.name === 'Perfil') {
+                iconName = 'person-circle-outline';
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={colorIcon} />;
+            },
+            headerStyle: {
+              backgroundColor: '#121212',
+            },
+            headerTitleStyle: {
+              color: '#FFF',
+            },
+            headerRight: () => (
+              <View style={{flexDirection: 'row', marginRight: 15}}>
+                <Ionicons
+                  name="cart"
+                  color="#FFF"
+                  size={25}
+                  style={{marginRight: 15}}
+                />
+                <Ionicons name="notifications" color="#FFF" size={25} />
+              </View>
+            ),
+            headerShadowVisible: false,
+            headerTitleAlign: 'center',
+            tabBarActiveTintColor: '#FFF',
+            tabBarInactiveTintColor: '#8F8F8F',
+            tabBarStyle: {
+              backgroundColor: '#1D1E1E',
+            },
+          })}>
           <Tab.Screen name="Carteiras" component={Wallets} />
           <Tab.Screen name="Portfolio" component={Portfolio} />
           <Tab.Screen name="Investir" component={Invest} />
--- src/pages/Contents/index.js
+++ src/pages/Contents/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const Contents = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Contents screen</Text>
+    <View style={styles.container}>
+      <Text style={{color: '#FFF'}}>Contents screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+  },
+});
 
 export default Contents;
