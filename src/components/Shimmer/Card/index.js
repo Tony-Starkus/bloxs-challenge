3da4ed0015c5907af6a35c49a7cdabcf3892e649
Tony-Starkus <thalisson.bandeira@hotmail.com>
2022-03-13 13:20:52 -0500

    refactor: recycle: cleaning code

--- src/components/Shimmer/Card/index.js
+++ src/components/Shimmer/Card/index.js
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import React from 'react';
 import {createShimmerPlaceholder} from 'react-native-shimmer-placeholder';
 import LinearGradient from 'react-native-linear-gradient';
